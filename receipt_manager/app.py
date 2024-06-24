361e3ff18160f711aae389abb80e401c2396dba1
B.H.J. <537423bl@student.eur.nl>
2024-06-25 00:04:04 +0200

    created ability to add additional products to a receipt

--- receipt_manager/app.py
+++ receipt_manager/app.py
@@ -1,18 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
-from database import db, Receipt, app
+from database import db, Receipt, Product, app
 
-# Route to home page
 @app.route('/')
 def index():
     return render_template('home.html')
 
-# Route to list all receipts
 @app.route('/receipts')
 def receipt_list():
     receipts = db.session.execute(db.select(Receipt).order_by(Receipt.date)).scalars()
     return render_template('receipt/list.html', receipts=receipts)
 
-# Route to create a new receipt
 @app.route('/receipts/create', methods=['GET', 'POST'])
 def receipt_create():
     if request.method == 'POST':
@@ -23,16 +20,26 @@ def receipt_create():
         )
         db.session.add(receipt)
         db.session.commit()
+        
+        product_index = 1
+        while f'product_name_{product_index}' in request.form:
+            product_name = request.form[f'product_name_{product_index}']
+            product_quantity = int(request.form[f'product_quantity_{product_index}'])
+            if product_name and product_quantity:
+                product = Product(name=product_name, quantity=product_quantity, receipt_id=receipt.id)
+                db.session.add(product)
+            product_index += 1
+        db.session.commit()
+        
         return redirect(url_for('receipt_list'))
     return render_template('receipt/create.html')
 
-# Route to view receipt details
 @app.route('/receipt/<int:id>')
 def receipt_detail(id):
     receipt = db.get_or_404(Receipt, id)
-    return render_template('receipt/detail.html', receipt=receipt)
+    products = Product.query.filter_by(receipt_id=id).all()
+    return render_template('receipt/detail.html', receipt=receipt, products=products)
 
-# Route to delete a receipt
 @app.route('/receipt/<int:id>/delete', methods=['GET', 'POST'])
 def receipt_delete(id):
     receipt = db.get_or_404(Receipt, id)
--- receipt_manager/database.py
+++ receipt_manager/database.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DigiBill.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///receipts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -11,6 +11,13 @@ class Receipt(db.Model):
     date = db.Column(db.String(50))
     company = db.Column(db.String(50))
     price = db.Column(db.Float)
+    products = db.relationship('Product', backref='receipt', lazy=True)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    receipt_id = db.Column(db.Integer, db.ForeignKey('receipt.id'), nullable=False)
 
 if __name__ == '__main__':
     with app.app_context():
